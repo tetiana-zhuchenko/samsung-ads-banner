e6a7de5a173146d04565888e2805a7249d31f1a5
Tania <taniyhamail@gmail.com>
2024-06-26 17:25:35 +0300

    feat: added slider animation

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-// var swiper = new Swiper('.mySwiper', {
-//   slidesPerView: 1,
-//   spaceBetween: 30,
-//   loop: true,
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// })
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+
+  navigation: {
+    // Navigation arrows
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+})
