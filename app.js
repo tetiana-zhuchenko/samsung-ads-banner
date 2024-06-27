9802b152a1d3b735a1cb9ed9c34bfc6066d5b7fb
Tania <taniyhamail@gmail.com>
2024-06-27 10:22:17 +0300

    feat: added completed slides animation. Used "Swiper"

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
+const swiperImg = new Swiper('.swiperImg', {
   direction: 'horizontal',
-
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+  },
   navigation: {
-    // Navigation arrows
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
     type: 'fraction',
+  },
+})
+
+const swiperText = new Swiper('.swiperText', {
+  direction: 'horizontal',
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+
+const swiperPagination = new Swiper('.swiperPagination', {
+  direction: 'horizontal',
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiperPagination-pagination',
   },
 })
